f55cc3a97888f2351de292fde5537af0087cd008
Tiffany Gan <tiffanygan2015@gmail.com>
2020-07-09 16:37:19 -0700

    add favicon and implement error message, clear button, and loader for calculator

--- loan-calculator/app.js
+++ loan-calculator/app.js
@@ -6,13 +6,40 @@ const numYears = document.getElementById('num-years');
 const monthlyPayment = document.getElementById('monthly-payment');
 const totPayment = document.getElementById('tot-payment');
 const totIntrest = document.getElementById('tot-intrest');
+const loader = document.getElementById('image');
+const error = document.getElementById('error');
+const clrBtn = document.getElementById('clr-btn');
 
-function calculateResults(e) {
-    e.preventDefault();
+function calculateResults() {
+    loader.style.display = 'block';
+    result.style.display = 'none';
+    setTimeout(afterShowingLoader, 1000);
+}
+
+function afterShowingLoader() {
+    loader.style.display = 'none'
     result.style.display = 'block';
-    monthlyPayment.value = loanAmount.value * Math.pow(1 + annualIntrest.value / 100, numYears.value) / 12 * numYears.value;
-    totPayment.value = loanAmount.value * Math.pow(1 + annualIntrest.value / 100, numYears.value);
-    totIntrest.value = totPayment.value - loanAmount.value;
+    if (!loanAmount.value) {
+        error.style.display = 'block';
+        result.style.display = 'none';
+        setTimeout(afterShowingError, 2000);
+    } else {
+        totPayment.value = (parseFloat(loanAmount.value) * Math.pow(1 + parseFloat(annualIntrest.value) / 100, parseFloat(numYears.value))).toFixed(2);
+        monthlyPayment.value = (parseFloat(totPayment.value) / (12 * parseFloat(numYears.value))).toFixed(2);
+        totIntrest.value = (parseFloat(totPayment.value) - parseFloat(loanAmount.value)).toFixed(2);
+    }
+}
+
+function afterShowingError() {
+    error.style.display = 'none';
+}
+
+function resetValues() {
+    loanAmount.value = '';
+    annualIntrest.value = '';
+    numYears.value = '';
+    result.style.display = 'none';
 }
 
 calculateBtn.addEventListener('click', calculateResults);
+clrBtn.addEventListener('click', resetValues);
