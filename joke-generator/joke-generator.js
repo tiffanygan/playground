f47580d6b67c7f372b399a4b3d03cf472e6dd909
Tiffany Gan <tiffanygan2015@gmail.com>
2020-07-20 20:40:39 -0700

    fix insecure http request issue

--- joke-generator/joke-generator.js
+++ joke-generator/joke-generator.js
@@ -3,7 +3,7 @@ document.getElementById('submit-btn').addEventListener('click', jokes);
 function jokes() {
     const number = document.querySelector('input').value;
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+    xhr.open('GET', `https://api.icndb.com/jokes/random/${number}`, true);
     xhr.addEventListener('load', function() {
         const xhrSimpleObject = JSON.parse(this.responseText);
         if (this.status === 200 && xhrSimpleObject.type === 'success') {
